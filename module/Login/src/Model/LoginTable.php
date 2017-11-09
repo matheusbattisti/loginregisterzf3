b1a53a93b8c8b2b85d9e70729c767e4dee3101e8
Matheus Battisti <matheusbudkewicz@gmail.com>
2017-11-09 18:54:02 -0200

    inserindo adaptador de DB

--- module/Login/src/Model/LoginTable.php
+++ module/Login/src/Model/LoginTable.php
@@ -4,6 +4,8 @@
 
 	use RuntimeException;
 	use Zend\Db\TableGateway\TableGatewayInterface;
+	use Zend\Authentication\AuthenticationService;
+	use Zend\Authentication\Adapter\DbTable\CredentialTreatmentAdapter as AuthAdapter;
 
 	class LoginTable
 	{
@@ -15,9 +17,37 @@
 			$this->tableGateway = $tableGateway;
 		}
 
-		public function authenticate()
+		public function authenticate(Login $login)
 		{
-			echo 'opa';
+			$authAdapter = new AuthAdapter(
+				$tableGateway, 
+				'users', 
+				'username', 
+				'password'
+			);
+
+			$data = [
+	            'username' => $login->username,
+	            'password'  => $login->password,
+	        ];
+
+	        $authAdapter
+	        	->setIdentity($data['username'])
+	        	->setCredential($data['password']);
+
+	        $result = $authAdapter->authenticate();
+
+	        if (! $result->isValid()) {
+			    // Authentication failed; print the reasons why:
+			    foreach ($result->getMessages() as $message) {
+			        echo "$message\n";
+			    }
+			} else {
+			    // Authentication succeeded; the identity ($username) is stored
+			    // in the session:
+			    // $result->getIdentity() === $auth->getIdentity()
+			    // $result->getIdentity() === $username
+			}
 		}
 
 	}
